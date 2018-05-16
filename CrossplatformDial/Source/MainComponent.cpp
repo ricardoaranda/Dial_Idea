8afddb4f972976c47ff0dff5b2c4ced436509495
ricardoaranda <ricardomessier@gmail.com>
2018-05-16 18:14:01 -0500

    Add files via upload

---
+++ CrossplatformDial/Source/MainComponent.cpp
@@ -0,0 +1,53 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    dial.setNormalisableRange (NormalisableRange<double> (0.0f, 1.0f, 0.01f));
+    dial.setSliderStyle (Slider::Rotary);
+    dial.setTextBoxStyle (Slider::NoTextBox, false, 0, 0);
+    dial.setLookAndFeel (&otherLookAndFeel);
+    
+    addAndMakeVisible (dial);
+    
+    setSize (500, 400);
+    setAudioChannels (2, 2);
+}
+
+MainComponent::~MainComponent()
+{
+    shutdownAudio();
+}
+
+//==============================================================================
+void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+{
+}
+
+void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
+{
+    bufferToFill.clearActiveBufferRegion();
+}
+
+void MainComponent::releaseResources()
+{
+}
+
+//==============================================================================
+void MainComponent::paint (Graphics& g)
+{
+    g.fillAll(Colours::black);
+}
+
+void MainComponent::resized()
+{
+    dial.setBounds (getLocalBounds().reduced(20));
+}
---
+++ CrossplatformDial/Source/MainComponent.h
@@ -0,0 +1,41 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "OtherLookAndFeel_V2.h"
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainComponent   : public AudioAppComponent
+{
+public:
+    //==============================================================================
+    MainComponent();
+    ~MainComponent();
+
+    //==============================================================================
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override;
+    void releaseResources() override;
+
+    //==============================================================================
+    void paint (Graphics& g) override;
+    void resized() override;
+
+private:
+    //==============================================================================
+    Slider dial;
+    OtherLookAndFeel_V2 otherLookAndFeel;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+};
